6e34b2b4be16327c1d68af080ca6871c21806aa3
melosomelo <mateusmelornrn@gmail.com>
2022-06-03 14:34:16 -0300

    implement resizing queue

--- linear-data-structures/Queue.java
+++ linear-data-structures/Queue.java
@@ -5,5 +5,72 @@ public class Queue {
     private int right = -1;
     private T[] array = (T[]) new Object[1];
 
+    public void print() {
+      String result = "[ ";
+      for (int i = 0; i < array.length; i++) {
+        if (array[i] == null) {
+          result += "- ";
+        } else {
+          result += array[i] + " ";
+        }
+      }
+      result += "]";
+      System.out.println(result);
+    }
+
+    private void resize(int newCapacity) {
+      T[] newArray = (T[]) new Object[newCapacity];
+      int size = size();
+      for (int i = 0; i < size; i++) {
+        newArray[i] = array[i + left];
+      }
+      array = newArray;
+      left = 0;
+      right = size - 1;
+    }
+
+    public void shiftLeft() {
+      int size = size();
+      for (int i = 0; i < size; i++) {
+        array[i] = array[i + left];
+      }
+      for (int i = size; i < array.length; i++) {
+        array[i] = null;
+      }
+      left = 0;
+      right = size - 1;
+    }
+
+    public void enqueue(T item) {
+      if (size() == array.length) {
+        resize(array.length * 2);
+      }
+      if (right == array.length - 1) {
+        shiftLeft();
+      }
+      array[++right] = item;
+    }
+
+    public T dequeue() throws Exception {
+      int size = size();
+      if (size == 0) {
+        throw new Exception("Cannot call dequeue on an empty queue!");
+      }
+      T item = array[left];
+      array[left++] = null;
+      if (size() == array.length / 4) {
+        resize(array.length / 2);
+      }
+      return item;
+    }
+
+    public boolean isEmpty() {
+      return size() == 0;
+    }
+
+    public int size() {
+      return right - left + 1;
+    }
   }
+
 }
